03c0222b2897fbec3f73121ad81e6e7c045defc2
santipawar <santipawar@gmail.com>
2017-12-17 19:34:33 -0500

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,22 @@
+
+
+library(shiny)
+shinyUI(fluidPage(
+  fluidRow(
+    headerPanel("Developing Data Products Week 4 Project"),
+    h3('Date: 17th Dec, 2017'),
+    titlePanel('Find geo-locations of places'),
+    helpText("Type the addresses/names of places to find their geo-location"),
+    h3('Input'),
+    helpText("Note: Input 1 location per line. A maximum of 10 locations can be queried at one time."),
+    HTML('<textarea id="addressArea" rows="10" cols="500">Orlando, FL, USA</textarea>'),
+    br(),
+    actionButton("goButton", "Done!"),
+    hr()
+  ),
+  fluidRow(
+    h3('Output'),
+    h4('Locations identified'),
+    tableOutput("locations")
+  )
+))
